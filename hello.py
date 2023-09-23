eddee0644d3b23d8beabeb7545570e8418e4f7ef
kb21032001 <kshitij.bhardwaj21@gmail.com>
2023-09-23 13:00:35 -0400

    Activity 4: Created web form and added name and email fields with validators.

--- hello.py
+++ hello.py
@@ -1,16 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+app.config['SECRET_KEY'] = 'you will never know'
 
-@app.route("/")
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT Email address?', validators=[DataRequired(), Email()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',
-                           current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+
+        old_email = session.get('email')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        session['name'] = form.name.data
+        session['email'] = form.email.data
+        session['mail_uoft'] = True if 'utoronto' in session['email'] else False
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'), mail_uoft=session.get('mail_uoft'))
 
 @app.route('/user/<name>')
 def user(name):
